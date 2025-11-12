dd690e88788307c7ed985f50d49fc7a462a48a31
Ana Paula Litholdo Ohama <analitholdo@br000c6dx674q52.ml.com>
2025-11-12 16:47:01 -0300

    Creating project

---
+++ src/main/java/com/analitholdo/partidafutebol/PartidaFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.analitholdo.partidafutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidaFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/analitholdo/partidafutebol/PartidaFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analitholdo.partidafutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidaFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
